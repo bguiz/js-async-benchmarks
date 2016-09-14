7febf3eacbb0d33a39fdebb4d1ce71d7d1fdcc56
bguiz <github@bguiz.com>
2016-09-14 18:45:21 +1000

    =BG= script to run the benchmarks

---
+++ run-benchmarks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export RUN=1
+export COUNT=${1}
+if [[ $COUNT =~ ^[-+]?[0-9]+$ ]] ; then
+	echo "count=${COUNT}"
+else
+	echo "must provide count as first argument"
+	exit 1
+fi
+
+. ~/.nvm/nvm.sh # get the `nvm` alias
+nvm use 4
+node ./print-version.js
+node ./benchmark-promises.js ${COUNT}
+node ./benchmark-callbacks.js ${COUNT}
+nvm use 6
+node ./print-version.js
+node ./benchmark-promises.js ${COUNT}
+node ./benchmark-callbacks.js ${COUNT}
